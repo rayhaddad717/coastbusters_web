41892931ad997475322748bf5a28263fb4b1cef6
Ray Haddad <rayghaddad@gmail.com>
2021-09-10 11:31:57 +0300

    fixed port

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.use((err, req, res, next) => {
     res.render('error', { err })
 })
 //Start Listening
-const port = process.env.Port ? process.env.port : 3000;
+const port = process.env.Port ? process.env.Port : 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}. It works`)
 });
