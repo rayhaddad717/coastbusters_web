177e11a9d2171ceac9926f908fddb91225b01195
Ray Haddad <rayghaddad@gmail.com>
2021-09-10 11:33:58 +0300

    fixed por

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.use((err, req, res, next) => {
     res.render('error', { err })
 })
 //Start Listening
-const port = process.env.Port ? process.env.Port : 3000;
+const port = process.env.PORT ? process.env.PORT : 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}. It works`)
 });
